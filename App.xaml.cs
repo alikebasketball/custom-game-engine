3fcf3f27b064525f98a86ae453b584fdfce51c1f
LordWrynn <44822503+LordWrynn@users.noreply.github.com>
2024-05-11 21:38:34 -0500

    Dotnet WPF template + GitIgnore
    Initial commit. Used Dotnet WPF and gitignore templates.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace custom_game_engine;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
